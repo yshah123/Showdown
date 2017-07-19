dc2da46fe73e0d2d8513e81d77983639554d3308
Sourav Biswas <biswasjosh@gmail.com>
2017-07-19 13:22:43 -0400

    Generates your random code

--- app/src/main/java/com/example/user/showdown/MainActivity.java
+++ app/src/main/java/com/example/user/showdown/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.user.showdown;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int randint = (int) (Math.random() * 1000000);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView codeTextView = (TextView) findViewById(R.id.your_random_code);
+        codeTextView.setText("" + randint);
     }
 }
